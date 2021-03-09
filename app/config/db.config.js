27d508ee63107b63aa0f87dcd9ed2d4b8b2ba674
eduardolibindo <eduardolibindo@gmail.com>
2021-03-09 09:43:30 -0300

    primeiro commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: A,
-    USER: A,
-    PASSWORD: A,
-    DB: D
+    HOST: "lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    USER: "gzc6816ovlhfmnss",
+    PASSWORD: "unhl9dewzvaz5k7w",
+    DB: "t7r14w6he7zlq8oh"
 }
